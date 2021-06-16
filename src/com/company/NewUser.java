f74317027b0841a7382d25974f50349f6d0ce12e
Bob <bjdhjy689@gmail.com>
2021-06-16 15:11:37 +0800

    777

--- src/com/company/NewUser.java
+++ src/com/company/NewUser.java
@@ -2,10 +2,10 @@ package com.company;
 
 // NewUser继承User
 public class NewUser extends User{
-    // TypeOfID域
+    // 子类的TypeOfID域
     String TypeOfID = "Driver's License";
 
-    // PrintMessage方法
+    // 子类的PrintMessage方法
     public void PrintMessage(){
         super.PrintMessage();
         System.out.println(super.TypeOfID);
--- src/com/company/User.java
+++ src/com/company/User.java
@@ -1,10 +1,10 @@
 package com.company;
 
 public class User {
-    // TypeOfID域
+    // 父类的TypeOfID域
     String TypeOfID = "Student Card";
 
-    // PrintMessage方法
+    // 父类的PrintMessage方法
     public void PrintMessage(){
         System.out.println("Hi!");
     }
